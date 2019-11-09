01fbc85e75a7a6e2216da9a17a6cd66d575b5bb1
Baup Salomé Sophie Célestine <baup@INTRANET.EPFL.CH@SVUbuUniv3.intranet.epfl.ch>
2019-11-09 10:08:09 +0100

    classe population

--- src/population.cpp
+++ src/population.cpp
@@ -1,2 +1,12 @@
-#include <population.hpp>
+#include "population.hpp"
 
+Population::Population(long int t)
+	: taille(t) 
+	{if (taille <= 0) {
+		throw std::runtime_error("Erreur! La taille de la population est inférieure à 0, le programme s'arrête.");
+		} //try et catch à faire dans main (ou lecture peut être)
+	} 
+	
+void Population::ajout_allele(std::string sequence, double frequence){
+	alleles.insert({sequence, frequence});
+}
