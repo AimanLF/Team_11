977ebbc4e3d845ccfac0a646aa6d74b869f0ef7f
Baup Salomé Sophie Célestine <baup@INTRANET.EPFL.CH@SVUbuUniv13.intranet.epfl.ch>
2019-11-10 22:04:56 +0100

    lien lecture-main

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
+#include <vector>
+#include <iostream>
+#include "lecture.hpp"
 #include <tclap/CmdLine.h>
 
 using namespace TCLAP;
 
 int main(int argc, char **argv){
 	
-	CmdLine cmd("TCLAP Random Number"); 
+	CmdLine cmd("TCLAP Genetique Populations");
+	 
+	ValueArg<std::string> fichier("f", "fichier", "nom du fichier fasta", true, "defaut" , "string" );
+	cmd.add(fichier);
+	MultiArg<size_t> marqueurs ("m", "marqueurs", "liste de marqueurs correspondant au fichier fourni" , false, "size_t" );
+	cmd.add(marqueurs);
 	
 	cmd.parse(argc, argv);
 	
+	std::vector<size_t> liste_marqueurs = marqueurs.getValue();
+	LiseurFasta lecteur(liste_marqueurs);
+	//lecteur.lire(fichier.getValue());   lire prend un istream en parametre, pas une string: le lecteur fournit un nom de fichier sous forme de string qu'il faudrait ensuite aller chercher en tant que fichier, en tant que istream? 
 	return 0;
 }
