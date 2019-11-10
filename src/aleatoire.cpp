94bd1c5a0b72c4ba45065d48aa7bda985a3f6add
Strasser Fanny Valentine <fstrasse@INTRANET.EPFL.CH@SVUbuUniv14.intranet.epfl.ch>
2019-11-10 15:59:37 +0100

    class aleatoire avec methode multinomiale

--- src/aleatoire.cpp
+++ src/aleatoire.cpp
@@ -1,2 +1,32 @@
+#include <iostream>
 #include "aleatoire.hpp"
+
+
+Aleatoire::Aleatoire () {
+    
+        std::random_device rd;
+        rng = std::mt19937(rd());
+}
+
+
+std :: vector<double> Aleatoire :: multinomiale(int N, std :: vector<double> effectif_allele){
+
+    std :: vector<double> nouveaux_effectifs(effectif_allele.size(), 0);
+
+    int k(0);
+    int taille(N);
+
+    for (size_t i(0); i < effectif_allele.size() - 1; ++i) {
+        std :: binomial_distribution<int> distribution(N + k, (double) effectif_allele[i]/taille);
+        nouveaux_effectifs[i] = distribution(rng);
+
+        k -= nouveaux_effectifs[i];
+        taille -= effectif_allele[i];
+    }
+
+    nouveaux_effectifs[nouveaux_effectifs.size() - 1] = N + k;
+
+
+    return nouveaux_effectifs;
+}
 
--- src/aleatoire.hpp
+++ src/aleatoire.hpp
@@ -1,1 +1,23 @@
+#pragma once
+
+#include <vector>
+#include <algorithm>
+#include <random>
+
+class Aleatoire{
+	
+	public : 
+	
+	Aleatoire (); 
+	std :: vector<double> multinomiale(int N, std :: vector<double> effectif_allele);
+	
+	private : 
+	std::mt19937 rng;
+
+	
+	
+	
+	
+	
+	};
 
