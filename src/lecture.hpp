b779a029782ec856368e4dc14da5d06123aa96dc
Simon Liétar <simon.lietar@epfl.ch>
2019-11-11 00:47:06 +0100

    Ajout de commentaires dans lecture.hpp

--- src/lecture.hpp
+++ src/lecture.hpp
@@ -10,13 +10,33 @@
 
 //chercher directement les position; verifier ACGT ??
 
+/**
+ * Classe pour lire les fichiers FASTA contenant les séquences d'ADN.
+ */
 class LiseurFasta {
 public:
-    LiseurFasta(std::vector<size_t> markers_ = { 2, 4 });
+    /**
+     * Constructeur. Prend en argument les indices de la séquence contenant les
+     * bases d'intérêt de chaque allèle.
+     */
+    LiseurFasta(std::vector<size_t> markers_);
+
+    /**
+     * Consomme la source et stocke et les allèles et fréquences respectives.
+     */
     void lire(std::istream& source);
-    //tableau de fréquences
+
+    /**
+     * Renvoie un tableau des fréquences de chaque allèles, dans le même ordre
+     * que alleles_seq().
+     */
     std::vector<double> frequence_alleles() const;
-    //tableau des séquences
+
+    /**
+     * Renvoie un tableau des allèles sous forme de texte. Tous les allèles ont
+     * la même longueur, c'est-à-dire le nombre de marqueurs donnés au
+     * constructeur.
+     */
     std::vector<std::string> alleles_seq() const;
   
 private :
