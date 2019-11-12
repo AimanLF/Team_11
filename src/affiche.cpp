7b3dadd031b4b6b8f2096e3edc87c77f7351ae80
jermann-e <56827475+jermann-e@users.noreply.github.com>
2019-11-12 20:05:23 +0100

    classe affiche pour fichier ou terminal

--- src/affiche.cpp
+++ src/affiche.cpp
@@ -3,10 +3,24 @@
 #include <iomanip>
 
 Afficheur::Afficheur(size_t nb_simul_)
-    :nb_simul(nb_simul_ - 1)
+        :nb_simul(nb_simul_ - 1)
 {
+    nomFichier = "genetique_population.txt";
+    monFlux = std::ofstream(nomFichier);
     compteur = 0;
 }
+
+void Afficheur::affiche_seq_fichier(std::vector<std::string> const& seq_alleles, size_t simul){
+    if(simul == 0) monFlux << std::setw(10);
+
+    size_t t(seq_alleles.size() - 1);
+    for (size_t j = 0; j < t; ++j) {
+        monFlux << seq_alleles[j] << '|';
+    }
+    monFlux << seq_alleles[t]<<std::setw(15);
+}
+
+
 
 void Afficheur::affiche_seq(std::vector<std::string> const& seq_alleles, size_t simul){
     if(simul == 0) std::cout << std::setw(10);
@@ -16,6 +30,22 @@ void Afficheur::affiche_seq(std::vector<std::string> const& seq_alleles, size_t 
         std::cout << seq_alleles[j] << '|';
     }
     std::cout << seq_alleles[t]<<std::setw(15);
+}
+
+void Afficheur::affiche_freq_fichier(std::vector<double> const& freq_alleles, size_t simul) {
+    if(simul == 0) {
+        monFlux << compteur_fichier << std::setw(10);
+        ++compteur_fichier;
+    }
+    size_t t(freq_alleles.size()-1);
+    for(size_t i=0; i < t ; ++i){
+        monFlux<<freq_alleles[i]<<'|';
+    }
+    monFlux<<freq_alleles[t];
+    if(simul < nb_simul) monFlux<< std::setw(10);
+
+    if(simul == nb_simul) monFlux<<std::endl;
+    //à la fin, comme la première ligne est 0
 }
 
 void Afficheur::affiche_freq(std::vector<double> const& freq_alleles, size_t simul) {
--- src/affiche.hpp
+++ src/affiche.hpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <fstream>
+
 /** Afficheur prend en attribut un size_t : le nombre R de simulations
  */
 class Afficheur {
@@ -25,9 +27,27 @@ public:
       * @param simul
       */
     void affiche_freq(std::vector<double> const& freq_alleles, size_t simul);
+
+    /** affiche_seq ecrivant dans un fichier texte
+     *
+     * @param seq_alleles
+     * @param simul
+     */
+    void affiche_seq_fichier(std::vector<std::string> const& seq_alleles, size_t simul);
+
+    /** affiche_freq ecrivant dans un fichier texte
+     *
+     * @param freq_alleles
+     * @param simul
+     */
+    void affiche_freq_fichier(std::vector<double> const& freq_alleles, size_t simul);
+
 private :
     int compteur;
+    int compteur_fichier;
     size_t nb_simul;
+    std::string nomFichier;
+    std::ofstream monFlux;
 };
 
 #endif
