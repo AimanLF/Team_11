3d010e6941296b8e7a07f9aab8e2dda4aae6005e
jermann-e <56827475+jermann-e@users.noreply.github.com>
2019-11-12 19:25:50 +0100

    affiche uniquement sur le terminal

--- src/affiche.cpp
+++ src/affiche.cpp
@@ -1,1 +1,35 @@
+
 #include "affiche.hpp"
+#include <iomanip>
+
+Afficheur::Afficheur(size_t nb_simul_)
+    :nb_simul(nb_simul_ - 1)
+{
+    compteur = 0;
+}
+
+void Afficheur::affiche_seq(std::vector<std::string> const& seq_alleles, size_t simul){
+    if(simul == 0) std::cout << std::setw(10);
+
+    size_t t(seq_alleles.size() - 1);
+    for (size_t j = 0; j < t; ++j) {
+        std::cout << seq_alleles[j] << '|';
+    }
+    std::cout << seq_alleles[t]<<std::setw(15);
+}
+
+void Afficheur::affiche_freq(std::vector<double> const& freq_alleles, size_t simul) {
+    if(simul == 0) {
+        std::cout << compteur << std::setw(10);
+        ++compteur;
+    }
+    size_t t(freq_alleles.size()-1);
+    for(size_t i=0; i < t ; ++i){
+        std::cout<<freq_alleles[i]<<'|';
+    }
+    std::cout<<freq_alleles[t];
+    if(simul < nb_simul) std::cout<< std::setw(10);
+
+    if(simul == nb_simul) std::cout<<std::endl;
+    //à la fin, comme la première ligne est 0
+}
--- src/affiche.hpp
+++ src/affiche.hpp
@@ -1,1 +1,33 @@
 
+#ifndef AFFICHE_H
+#define AFFICHE_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+/** Afficheur prend en attribut un size_t : le nombre R de simulations
+ */
+class Afficheur {
+public:
+    Afficheur(size_t nb_simul_);
+    /** affiche_seq prend en attribut la liste d'alleles à afficher: un tableau de string
+     * et la simulation actuelle.
+     * /
+     * @param seq_alleles
+     * @param simul
+     */
+    void affiche_seq(std::vector<std::string> const& seq_alleles, size_t simul);
+     /** affiche_freq prend en attribut la liste des fréquences à afficher: un tableau de double
+     * et la simulation actuelle.
+      * /
+      * @param freq_alleles
+      * @param simul
+      */
+    void affiche_freq(std::vector<double> const& freq_alleles, size_t simul);
+private :
+    int compteur;
+    size_t nb_simul;
+};
+
+#endif
